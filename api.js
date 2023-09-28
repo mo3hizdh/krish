371313bec2428ef5b51bd816250f7618cb3c1ba8
mo3hizdh <146360296+mo3hizdh@users.noreply.github.com>
2023-09-28 20:26:58 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+var muscle = 'biceps'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
+    headers: { 'X-Api-Key': 'k7Cz+bS9DrI0XennMvISMQ==C5w1eXp4j85dDJIz'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
